38efd469a6f56dff0b720e49da8acd5f06043ff8
TarrinRos <tarindis@gmail.com>
2019-01-14 16:06:52 +0200

    Add questions controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,7 @@ Rails.application.routes.draw do
     put 'answer', on: :member # доп. метод ресурса - ответ на текущий вопро
     put 'take_money', on: :member # доп. метод ресурса - игрок берет деньги
   end
+
+  # Ресурс вопросов в единственном числе, для загрузки админом пачки вопросов
+  resource :questions, only: [:new, :create]
 end
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
+# Наш собственный класс с вспомогательными методами
+require 'support/my_spec_helper'
+
 RSpec.describe GamesController, type: :controller do
   let(:user) { FactoryBot.create(:user) }
   let(:admin) { FactoryBot.create(:user, is_admin: true) }
@@ -12,6 +15,51 @@ RSpec.describe GamesController, type: :controller do
       expect(response.status).to_not eq 200
       expect(response).to redirect_to(new_user_session_path)
       expect(flash[:alert]).to be
+    end
+  end
+
+# группа тестов на экшены контроллера, доступных залогиненным юзерам
+  context 'Usual user' do
+    # перед каждым тестом в группе
+    before(:each) { sign_in user } # логиним юзера user с помощью спец. Devise метода sign_in
+
+    # юзер может создать новую игру
+    it 'creates game' do
+      # сперва накидаем вопросов, из чего собирать новую игру
+      generate_questions(15)
+
+      post :create
+      game = assigns(:game) # вытаскиваем из контроллера поле @game
+
+      # проверяем состояние этой игры
+      expect(game.finished?).to be_falsey
+      expect(game.user).to eq(user)
+      # и редирект на страницу этой игры
+      expect(response).to redirect_to(game_path(game))
+      expect(flash[:notice]).to be
+    end
+
+    # юзер видит свою игру
+    it '#show game' do
+      get :show, id: game_w_questions.id
+      game = assigns(:game) # вытаскиваем из контроллера поле @game
+      expect(game.finished?).to be_falsey
+      expect(game.user).to eq(user)
+
+      expect(response.status).to eq(200) # должен быть ответ HTTP 200
+      expect(response).to render_template('show') # и отрендерить шаблон show
+    end
+
+    # юзер отвечает на игру корректно - игра продолжается
+    it 'answers correct' do
+      # передаем параметр params[:letter]
+      put :answer, id: game_w_questions.id, letter: game_w_questions.current_game_question.correct_answer_key
+      game = assigns(:game)
+
+      expect(game.finished?).to be_falsey
+      expect(game.current_level).to be > 0
+      expect(response).to redirect_to(game_path(game))
+      expect(flash.empty?).to be_truthy # удачный ответ не заполняет flash
     end
   end
 end
