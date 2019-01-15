2df55198b951f955dfbf8c8d0fc724d87f0528c6
TarrinRos <tarindis@gmail.com>
2019-01-15 11:22:08 +0200

    Add correct audience_help test

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -37,4 +37,24 @@ RSpec.describe GameQuestion, type: :model do
       expect(game_question.correct_answer_key).to eq 'b'
     end
   end
+
+  # help_hash у нас имеет такой формат:
+  # {
+  #   fifty_fifty: ['a', 'b'], # При использовании подсказски остались варианты a и b
+  #   audience_help: {'a' => 42, 'c' => 37 ...}, # Распределение голосов по вариантам a, b, c, d
+  #   friend_call: 'Василий Петрович считает, что правильный ответ A'
+  # }
+
+  context 'user helpers' do
+    it 'correct audience_help' do
+      expect(game_question.help_hash).not_to include(:audience_help)
+
+      game_question.add_audience_help
+
+      expect(game_question.help_hash).to include(:audience_help)
+
+      ah = game_question.help_hash[:audience_help]
+      expect(ah.keys).to contain_exactly('a', 'b', 'c', 'd')
+    end
+  end
 end
