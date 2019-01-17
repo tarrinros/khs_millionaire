be5260999652daa5eb4b456701c369129354711d
TarrinRos <tarindis@gmail.com>
2019-01-17 14:54:42 +0200

    Add Ajax for the game and some test for views

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -57,13 +57,27 @@ class GamesController < ApplicationController
       )
     end
 
-    if @game.finished?
-      # Если игра закончилась, отправялем юзера на свой профиль
-      redirect_to user_path(current_user)
-    else
-      # Иначе, обратно на экран игры
-      redirect_to game_path(@game)
+    # Выбираем поведение в зависимости от формата запроса
+    respond_to do |format|
+      # Если это html-запрос, по-старинке редиректим пользователя в зависимости
+      # от ситуации
+      format.html do
+        if @answer_is_correct && !@game.finished?
+          redirect_to game_path(@game)
+        else
+          redirect_to user_path(current_user)
+        end
+      end
+
+      # Если это js-запрос, то ничего не делаем и контролл попытается отрисовать
+      # шаблон
+      #
+      # <controller>/<action>.<format>.erb
+      #
+      # В нашем случае будет games/answer.js.erb
+      format.js {}
     end
+
   end
 
   # Действие take_money вызывается из шаблона, когда пользователь берет кнопку
@@ -93,6 +107,7 @@ class GamesController < ApplicationController
 
     redirect_to game_path(@game), msg
   end
+
 
   private
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,12 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show]
 
   def index
+    # Для страницы рейтинга нам понадобятся все пользователи, остортированные по выигрышу
     @users = User.all.order(balance: :desc)
   end
 
   def show
+    # Для профиля пользователя нам понадобятся всего игры в порядке давности
     @games = @user.games.order(created_at: :desc)
   end
 
