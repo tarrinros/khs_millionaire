80b4eb2ff503ea0fde2eae15fdecd4c7e15414cf
TarrinRos <tarindis@gmail.com>
2019-02-08 17:32:27 +0200

    Add gem to the feature test for users profile

--- spec/features/user_looks_at_another_profile_spec.rb
+++ spec/features/user_looks_at_another_profile_spec.rb
@@ -1,17 +1,32 @@
 require 'rails_helper'
 
 RSpec.feature 'USER looks at another profile', type: :feature do
-  let(:user) do
-      FactoryBot.create :user
+  let!(:user) do
+      FactoryBot.create :user, name: 'Alex', balance: 5000
   end
+
 
   # before(:each) do
   #   login_as user
   # end
 
+  before(:each) do
+    FactoryBot.create(:game, user_id: user.id)
+  end
+
+  scenario 'successfully' do
+    visit "/"
+
+    click_link 'Alex'
+
+    save_and_open_page
+
+    # expect(page).to have_content 'Alex'
+  end
+
   scenario 'successfully' do
     visit "users/#{user.id}"
 
-
+    expect(page).to have_content 'Alex'
   end
 end
