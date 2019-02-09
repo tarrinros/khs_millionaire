c441294fe9a0bc81dda947b8bc93c4cf30bcd67d
TarrinRos <tarindis@gmail.com>
2019-02-09 15:21:31 +0200

    Refactor feature test for users profile

--- spec/features/user_looks_at_another_profile_spec.rb
+++ spec/features/user_looks_at_another_profile_spec.rb
@@ -19,11 +19,12 @@ RSpec.feature 'USER looks at another profile', type: :feature do
 
     expect(page).to have_current_path '/users/1'
     expect(page).to have_content 'Alex'
-    expect(page).to have_content 'проигрыш'
     expect(page).to have_content '1'
-    expect(page).to have_content '5'
-    expect(page).to have_content '1 000 ₽'
-    expect(page).to have_content '17 янв., 13:53'
+    expect(page).to have_content '2'
+    expect(page).to have_content '3'
+    expect(page).to have_content '4 проигрыш 17 янв., 13:53 5 1 000 ₽'
     expect(page).to_not have_content 'Сменить имя и пароль'
+
+    save_and_open_page
   end
 end
