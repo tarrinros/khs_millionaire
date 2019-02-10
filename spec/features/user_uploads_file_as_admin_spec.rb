aa6becd2fa45b637feb5a159c0e7017cefe6f9e9
TarrinRos <tarindis@gmail.com>
2019-02-10 22:50:53 +0200

    Add feature test for uploading files by admin

---
+++ spec/features/user_uploads_file_as_admin_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature 'USER uploads file as admin', type: :feature do
+  let!(:user) do
+    FactoryBot.create :user, name: 'Alex', is_admin: true
+  end
+
+  before(:each) do
+    login_as user
+  end
+
+  scenario 'successfully' do
+    visit "/"
+
+    click_link 'Залить новые вопросы'
+
+    fill_in 'questions_level', with: '7'
+
+    page.attach_file('questions_file', Rails.root + 'public/7.txt')
+
+    click_button 'Save changes'
+
+    expect(page).to have_current_path '/questions/new'
+    expect(page).to have_content 'Уровень 7, обработано 477, создано 474, время 02.000 сек'
+  end
+end
