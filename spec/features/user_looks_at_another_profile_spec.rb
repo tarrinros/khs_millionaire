e061d086e0a4b7bab7a6fe3633b982d45e652c3d
TarrinRos <tarindis@gmail.com>
2019-02-10 13:13:05 +0200

    Fix feature test for users profile by uniq strings

--- spec/features/user_looks_at_another_profile_spec.rb
+++ spec/features/user_looks_at_another_profile_spec.rb
@@ -2,14 +2,19 @@ require 'rails_helper'
 
 RSpec.feature 'USER looks at another profile', type: :feature do
   let!(:user) do
-      FactoryBot.create :user, name: 'Alex', balance: 5000
+    FactoryBot.create :user, name: 'Alex', balance: 2000, is_admin: false
   end
 
-  before(:each) do
-    FactoryBot.create_list(:game, 5, user_id: user.id, prize: 1000, current_level: 5, is_failed: true,
-                           created_at: Time.parse('2019-01-17 12:53:38'),
-                           updated_at: Time.parse('2019-01-17 12:53:38'),
-                           finished_at: Time.parse('2019-01-17 12:53:38'))
+  let!(:games) do
+    [
+      FactoryBot.create(:game, id: 1, user_id: user.id, prize: 64000, current_level: 11, is_failed: false,
+                        created_at: Time.parse('2018-08-19 22:37:58'),
+                        updated_at: Time.parse('2018-08-19 22:47:44'),
+                        finished_at: Time.parse('2018-08-19 22:47:44')),
+
+      FactoryBot.create(:game, id: 2, user_id: user.id, prize: 1000, current_level: 5, is_failed: false,
+                        created_at: Time.parse('2018-09-30 22:44:58'))
+    ]
   end
 
   scenario 'successfully' do
@@ -19,10 +24,8 @@ RSpec.feature 'USER looks at another profile', type: :feature do
 
     expect(page).to have_current_path '/users/1'
     expect(page).to have_content 'Alex'
-    expect(page).to have_content '1'
-    expect(page).to have_content '2'
-    expect(page).to have_content '3'
-    expect(page).to have_content '4 проигрыш 17 янв., 13:53 5 1 000 ₽'
+    expect(page).to have_content '1 деньги 19 авг., 22:37 11 64 000 ₽'
+    expect(page).to have_content '2 в процессе 30 сент., 22:44 5 1 000 ₽'
     expect(page).to_not have_content 'Сменить имя и пароль'
 
     save_and_open_page
